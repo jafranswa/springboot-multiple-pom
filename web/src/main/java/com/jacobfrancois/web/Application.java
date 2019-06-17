5a3015ca2481bc76384124edd8dbdc915b12ec2b
jacob <jafranswa@gmail.com>
2019-06-17 17:14:24 -0500

    maven builds successfully

--- web/src/main/java/com/jacobfrancois/web/Application.java
+++ web/src/main/java/com/jacobfrancois/web/Application.java
@@ -3,8 +3,12 @@ package com.jacobfrancois.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.jacobfrancois.persistance")
+@EntityScan(basePackages = "com.jacobfrancois.domain")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
